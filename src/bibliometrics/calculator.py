4cb5cc1188bcc01ed00cfe188825e1bb9925a57a
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2024-02-25 15:31:50 -0500

    Refactored bibliometric calculations for improved maintainability (#63)
    * refactor bibliometric calculations
    
    * refactor bibliometric calculations
    
    * examples update
    
    * refactor
    
    * refactor
    
    * Update CHANGELOG.md

---
+++ src/bibliometrics/calculator.py
@@ -0,0 +1,175 @@
+# bibliometrics: Summarize your Google Scholar bibliometrics in an SVG
+# 
+# Copyright (c) 2022-2024 Vincent A Cicirello
+# https://www.cicirello.org/
+#
+# MIT License
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+import math
+
+class BibliometricCalculator:
+    """Calculates the various bibliometrics."""
+
+    __slots__ = [ '_metrics' ]
+
+    def __init__(self, metrics, cites_list):
+        """Initializes the BibliometricCalculator.
+
+        Keyword arguments:
+        metrics - a dict of the metrics scraped directly from Scholar profile
+        cites_list - a list of the citations of articles scraped from profile
+        """
+        self._metrics = dict(metrics)
+        if "h-index" not in self._metrics or len(cites_list) == 0:
+            return
+        sorted_cites = sorted(cites_list, reverse=True)
+        if sorted_cites[0] <= 0:
+            return
+        self._calculate_most(sorted_cites)
+        self._calculate_o_index()
+        self._calculate_g_index(sorted_cites)
+        self._calculate_h_median(sorted_cites)
+        h_core_sum = self._calculate_h_core_citations(sorted_cites)
+        self._calculate_e_index(h_core_sum)
+        self._calculate_R_index(h_core_sum)
+        self._calculate_A_index(h_core_sum)
+        self._calculate_ixx_index(sorted_cites, 100)
+        self._calculate_ixx_index(sorted_cites, 1000)
+        self._calculate_ixx_index(sorted_cites, 10000)
+
+    def to_dict(self):
+        """Returns a dict of the bibliometrics."""
+        return dict(self._metrics)
+
+    def _calculate_most(self, sorted_cites):
+        """Initializes the most cited.
+
+        Keyword arguments:
+        sorted_cites - a list of the citations per publication sorted decreasing
+        """
+        self._metrics["most-cited"] = sorted_cites[0]
+
+    def _calculate_o_index(self):
+        """Calculates the o-index, which is the geometric mean
+        of the h-index and the number of citations to the most-cited
+        paper.
+        """
+        if "most-cited" in self._metrics:
+            self._metrics["o-index"] = round(
+                math.sqrt(
+                    self._metrics["h-index"] * self._metrics["most-cited"]
+                )
+            )
+
+    def _calculate_g_index(self, sorted_cites) :
+        """Calculates the g-index.
+
+        Keyword arguments:
+        sorted_cites - List of citations of papers in decreasing order.
+        """
+        rolling_sum = [ sorted_cites[0] ]
+        for i in range(1, len(sorted_cites)) :
+            rolling_sum.append(sorted_cites[i] + rolling_sum[i-1])
+        rolling_sum = [ (i+1, x) for i, x in enumerate(rolling_sum) ]
+        g = max(y for y, x in rolling_sum if x >= y*y)
+        if g > 0 and g < 100 :
+            self._metrics["g-index"] = g
+
+    def _calculate_h_median(self, sorted_cites) :
+        """Calculates the median number of citations to publications in
+        the h-core, i.e., the h most-cited papers.
+
+        Keyword arguments:
+        sorted_cites - List of citations of papers in decreasing order.
+        """
+        h = self._metrics["h-index"]
+        if h >= 200:
+            return
+        if h % 2 == 0:
+            m1 = h // 2
+            if m1 >= len(sorted_cites):
+                return
+            m0 = m1 - 1
+            total = sorted_cites[m0] + sorted_cites[m1]
+            median = total // 2 if total % 2 == 0 else total / 2
+        else:
+            m = h // 2
+            median = sorted_cites[m] if m < len(sorted_cites) else 0
+        if median > 0.0:
+            self._metrics["h-median"] = median if (
+                isinstance(median, int)) else "{0:.1f}".format(median)
+
+    def _calculate_h_core_citations(self, sorted_cites) :
+        """Calculates the total number of citations to the publications
+        in the h-core, i.e., the h most-cited papers.
+
+        Keyword arguments:
+        sorted_cites - List of citations of papers in decreasing order.
+        """
+        if self._metrics["h-index"] > 100:
+            return 0
+        if len(sorted_cites) < self._metrics["h-index"]:
+            return 0
+        return sum(sorted_cites[i] for i in range(self._metrics["h-index"]))
+
+    def _calculate_e_index(self, h_core_sum) :
+        """Calculates the e-index.
+
+        Keyword arguments:
+        h_core_sum - sum of the citations to the h publications in the h-core.
+        """
+        h = self._metrics["h-index"]
+        e = math.sqrt(h_core_sum - h*h) if h <= 100 else 0
+        if e > 0.0 :
+            self._metrics["e-index"] = "{0:.2f}".format(e)
+
+    def _calculate_R_index(self, h_core_sum) :
+        """Calculates the R-index.
+
+        Keyword arguments:
+        h_core_sum - sum of the citations to the h publications in the h-core.
+        """
+        r = math.sqrt(h_core_sum) if self._metrics["h-index"] <= 100 else 0
+        if r > 0.0 :
+            self._metrics["r-index"] = "{0:.2f}".format(r)
+
+    def _calculate_A_index(self, h_core_sum) :
+        """Calculates the A-index.
+
+        Keyword arguments:
+        h_core_sum - sum of the citations to the h publications in the h-core.
+        """
+        h = self._metrics["h-index"]
+        a = h_core_sum / h if h > 0 and h <= 100 else 0
+        if a > 0.0 :
+            self._metrics["a-index"] = "{0:.2f}".format(a)
+
+    def _calculate_ixx_index(self, sorted_cites, xx):
+        """Calculates i100, i1000, etc.
+
+        Keyword arguments:
+        sorted_cites - List of citations of papers in decreasing order.
+        xx - 100 for i100-index, 1000 for i1000-index, etc.
+        """
+        ixx = sum(1 for y in sorted_cites if y >= xx)
+        if ixx > 0 and ixx < 100:
+            self._metrics["i{0}-index".format(xx)] = ixx
--- tests/tests.py
+++ tests/tests.py
@@ -29,6 +29,7 @@ import unittest
 import sys, math
 sys.path.insert(0,'src')
 import bibliometrics.bibliometrics as bib
+from bibliometrics.calculator import BibliometricCalculator
 
 class TestBibiometrics(unittest.TestCase) :
 
@@ -36,64 +37,188 @@ class TestBibiometrics(unittest.TestCase) :
     # change this to True.
     printSampleImage = False
 
+    def test_calculator_retains_scraped(self):
+        metrics = {
+            "total-cites" : 42,
+            "five-year-cites" : 6,
+            "h-index" : 3,
+            "i10-index" : 1
+        }
+        calc = BibliometricCalculator(metrics, [])
+        self.assertEqual(metrics, calc._metrics)
+        calc = BibliometricCalculator(metrics, [5, 20, 9])
+        for key, value in metrics.items():
+            self.assertEqual(value, calc._metrics[key])
+
+    def test_calculate_most(self):
+        metrics = {
+            "total-cites" : 42,
+            "five-year-cites" : 6,
+            "h-index" : 3,
+            "i10-index" : 1
+        }
+        calc = BibliometricCalculator(metrics, [5, 20, 9])
+        self.assertEqual(20, calc._metrics["most-cited"])
+
     def test_calculate_o_index(self):
-        self.assertEqual(42, bib.calculate_o_index(42, 42))
-        self.assertEqual(8, bib.calculate_o_index(1, 64))
-        self.assertEqual(8, bib.calculate_o_index(64, 1))
-        self.assertEqual(8, bib.calculate_o_index(2, 32))
-        self.assertEqual(8, bib.calculate_o_index(32, 2))
+        metrics = {
+            "total-cites" : 4200,
+            "five-year-cites" : 6,
+            "h-index" : 42,
+            "i10-index" : 1
+        }
+        self.assertEqual(42, BibliometricCalculator(metrics, [42]*42)._metrics["o-index"])
+        metrics["h-index"] = 1
+        self.assertEqual(8, BibliometricCalculator(metrics, [5, 20, 64])._metrics["o-index"])
+        metrics["h-index"] = 2
+        self.assertEqual(8, BibliometricCalculator(metrics, [5, 20, 32])._metrics["o-index"])
+        metrics["h-index"] = 0
+        self.assertEqual(0, BibliometricCalculator(metrics, [1, 0, 2])._metrics["o-index"])
 
-    def test_calculate_h_core_citations(self):
-        cites = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-        for h in range(1, 1+len(cites)):
-            expected = 55 - (10-h)*(11-h)//2
-            self.assertEqual(expected, bib.calculate_h_core_citations(cites, h))
-
-    def test_calculate_h_median(self):
-        cites = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-        expected= [
-            10, 9.5, 9, 8.5, 8, 7.5, 7, 6.5, 6,
-            5.5, 5, 4.5, 4, 3.5, 3, 2.5, 2, 1.5, 1, 0]
-        for i in range(len(expected)):
-            h = i + 1
-            self.assertEqual(expected[i], bib.calculate_h_median(cites, h))
-            
     def test_calculate_g(self) :
+        metrics = {
+            "total-cites" : 4200,
+            "five-year-cites" : 6,
+            "h-index" : 1,
+            "i10-index" : 1
+        }
         for g in range(1, 11) :
             cites = [10]*g
-            self.assertEqual(g, bib.calculate_g_index(cites))
+            self.assertEqual(g, BibliometricCalculator(metrics, cites)._metrics["g-index"])
         for g in range(11, 21) :
             cites = [10]*g
-            self.assertEqual(10, bib.calculate_g_index(cites))
+            self.assertEqual(10, BibliometricCalculator(metrics, cites)._metrics["g-index"])        
 
-    def test_calculate_e_no_excess(self) :
+    def test_calculate_h_median(self):
+        metrics = {
+            "total-cites" : 4200,
+            "five-year-cites" : 6,
+            "h-index" : 42,
+            "i10-index" : 1
+        }
+        cites = [30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11]
+        expected= [
+            30, 29.5, 29, 28.5, 28, 27.5, 27, 26.5, 26,
+            25.5, 25, 24.5, 24, 23.5, 23, 22.5, 22, 21.5, 21, 20.5]
+        for i in range(len(expected)):
+            metrics["h-index"] = i + 1
+            if expected[i] > 0:
+                h_median = BibliometricCalculator(metrics, cites)._metrics["h-median"]
+                self.assertEqual(
+                    expected[i],
+                    float(h_median) if isinstance(h_median, str) else h_median
+                )
+            else:
+                self.assertFalse("h-median" in BibliometricCalculator(metrics, cites)._metrics)
+    
+    def test_calculate_h_core_citations(self):
+        metrics = {
+            "total-cites" : 4200,
+            "five-year-cites" : 6,
+            "h-index" : 42,
+            "i10-index" : 1
+        }
+        cites = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
+        for h in range(1, 1+len(cites)):
+            metrics["h-index"] = h
+            expected = 10 * (55 - (10-h)*(11-h)//2)
+            self.assertEqual(
+                expected,
+                BibliometricCalculator(metrics, cites)._calculate_h_core_citations(cites)
+            )
+
+    def test_calculate_e_no_excess(self):
+        metrics = {
+            "total-cites" : 4200,
+            "five-year-cites" : 6,
+            "h-index" : 42,
+            "i10-index" : 1
+        }
         for h in range(0, 10) :
-            h_core_sum = h*h
-            self.assertEqual(0.0, bib.calculate_e_index(h_core_sum, h))
-
+            metrics["h-index"] = h
+            cites = [h]*20
+            self.assertFalse("e-index" in BibliometricCalculator(metrics, cites)._metrics)
+            
     def test_calculate_e_equal_excess(self) :
-        for h in range(0, 10) :
-            h_core_sum = h*h + 5*h
-            self.assertEqual(math.sqrt(5*h), bib.calculate_e_index(h_core_sum, h))
+        metrics = {
+            "total-cites" : 4200,
+            "five-year-cites" : 6,
+            "h-index" : 42,
+            "i10-index" : 1
+        }
+        for h in range(1, 10) :
+            metrics["h-index"] = h
+            cites = [h+5]*h + [1]*5
+            self.assertAlmostEqual(
+                math.sqrt(5*h),
+                float(BibliometricCalculator(metrics, cites)._metrics["e-index"]),
+                places=2
+            )
 
     def test_calculate_e_unequal_excess(self) :
-        for h in range(0, 10) :
-            h_core_sum = sum(h+x for x in range(h, 0, -1))
-            self.assertEqual(math.sqrt(h*(h+1)/2), bib.calculate_e_index(h_core_sum, h))
+        metrics = {
+            "total-cites" : 4200,
+            "five-year-cites" : 6,
+            "h-index" : 42,
+            "i10-index" : 1
+        }
+        for h in range(1, 10) :
+            metrics["h-index"] = h
+            cites = [h+x for x in range(h, 0, -1)]
+            self.assertAlmostEqual(
+                math.sqrt(h*(h+1)/2),
+                float(BibliometricCalculator(metrics, cites)._metrics["e-index"]),
+                places=2
+            )
 
     def test_calculate_R_index(self):
-        inputs = [100, 81, 64, 4, 1, 0]
-        outputs = [10, 9, 8, 2, 1, 0]
+        metrics = {
+            "total-cites" : 4200,
+            "five-year-cites" : 6,
+            "h-index" : 42,
+            "i10-index" : 1
+        }
+        inputs = [100, 81, 64, 4, 1]
+        outputs = [10, 9, 8, 2, 1]
         for h_core_sum, expected in zip(inputs, outputs):
-            self.assertEqual(expected, bib.calculate_R_index(h_core_sum))
+            metrics["h-index"] = 1
+            cites = [h_core_sum] + [0]*5
+            self.assertEqual(
+                expected,
+                float(BibliometricCalculator(metrics, cites)._metrics["r-index"])
+            )
 
     def test_calculate_A_index(self):
+        metrics = {
+            "total-cites" : 4200,
+            "five-year-cites" : 6,
+            "h-index" : 42,
+            "i10-index" : 1
+        }
         h_core_sum = 100
-        inputs = [1, 2, 4, 5, 20, 25, 50, 100]
-        outputs = [100, 50, 25, 20, 5, 4, 2, 1]
+        inputs = [1, 2, 4, 5]
+        outputs = [100, 50, 25, 20]
         for h, expected in zip(inputs, outputs):
-            self.assertEqual(expected, bib.calculate_A_index(h_core_sum, h))
-        
+            metrics["h-index"] = h
+            cites = [h_core_sum // h]*h + [0]*5
+            self.assertEqual(
+                expected,
+                float(BibliometricCalculator(metrics, cites)._metrics["a-index"])
+            )
+
+    def test_calculate_ixx_index(self):
+        metrics = {
+            "total-cites" : 4200,
+            "five-year-cites" : 6,
+            "h-index" : 9,
+            "i10-index" : 1
+        }
+        cites = [10001, 10000, 1002, 1001, 1000, 103, 102, 101, 100, 5, 4, 3, 2, 1]
+        calc = BibliometricCalculator(metrics, cites)
+        self.assertEqual(2, calc._metrics["i10000-index"])
+        self.assertEqual(5, calc._metrics["i1000-index"])
+        self.assertEqual(9, calc._metrics["i100-index"])
+
     def test_parse(self) :
         with open("tests/testcase.html.txt", "r") as f :
             page = f.read().replace('\n', '')
